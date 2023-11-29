aef2c860bc4c22e17ca199cd76f5869aeafd32e4
ShahriarEmon3215 <shahriar3215emon@gmail.com>
2023-11-29 20:38:33 +0600

    latest code

---
+++ lib/3D_animations.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Flutter3DAnimations extends StatefulWidget {
+  const Flutter3DAnimations({super.key});
+
+  @override
+  State<Flutter3DAnimations> createState() => _Flutter3DAnimationsState();
+}
+
+class _Flutter3DAnimationsState extends State<Flutter3DAnimations> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox( 
+       
+      ),
+    );
+  }
+}
